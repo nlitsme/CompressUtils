84fe7ca1efe1360c010ce6804f7d46fedde6704f
itsme <itsme@xs4all.nl>
2010-05-10 23:11:03 +0200

    added ctrl-c handling

--- testipc.cpp
+++ testipc.cpp
@@ -10,6 +10,8 @@
 #include "boost_ipc_clt.h"
 #elif defined(USE_SHMEM)
 #include "shmem_ipc_clt.h"
+#elif defined(USE_PTY)
+#include "pty_ipc_clt.h"
 #endif
 
 #include <stdio.h>
@@ -19,6 +21,14 @@
 #include "vectorutils.h"
 #include "util/HiresTimer.h"
 
+int g_ctrlcpressed= 0;
+void handlectrlc(int sig)
+{
+    fprintf(stderr, "ctrl-c pressed\n");
+    if (g_ctrlcpressed++ > 2)
+        signal(sig, SIG_DFL);
+}
+
 template<typename T>
 bool runtest(T& ipc, int size)
 {
@@ -27,13 +37,17 @@ bool runtest(T& ipc, int size)
     *(DWORD*)&req[4]= size;
 
     ByteVector res(size);
-    for (int i=0 ; i<10000 ; i++)
+    for (int i=0 ; !g_ctrlcpressed && i<10000 ; i++)
     {
         if (!ipc.write(&req[0], req.size()))
             return false;
 
+        if (g_ctrlcpressed)
+            break;
+
         if (!ipc.read(&res[0], res.size()))
             return false;
+        fprintf(stderr, "got svr answer\n");
     }
 
     return true;
@@ -53,14 +67,16 @@ void do_client(const std::string& svrname, const StringList& args)
     boost_ipc_client _ipc(svrname, args);
 #elif defined(USE_SHMEM)
     shmem_ipc_client _ipc(svrname, args);
+#elif defined(USE_PTY)
+    pty_ipc_client _ipc(svrname, args);
 #endif
 
-    for (int size= 0x100 ; size<0x100000 ; size<<=1)
+    for (int size= 0x100 ; !g_ctrlcpressed && size<0x100000 ; size<<=1)
     {
         HiresTimer t;
         if (!runtest(_ipc, size))
-            printf("test for size %d failed\n", size);
-        printf("%08x -> %6d\n", size, t.msecelapsed());
+            fprintf(stderr, "test for size %d failed\n", size);
+        fprintf(stderr, "%08x -> %6d\n", size, t.msecelapsed());
     }
 }
 
@@ -82,12 +98,21 @@ void do_client(const std::string& svrname, const StringList& args)
 #define SVRNAME  "./testshmemsvr"
 #endif
 
+#elif defined(USE_PTY)
+
+#ifdef _WIN32
+#define SVRNAME  "testptysvr.exe"
+#else
+#define SVRNAME  "./testptysvr"
+#endif
+
 #else
 #define SVRNAME  "./teststdiosvr"
 #endif
 
 int main(int argc,char**argv)
 {
+    signal(SIGINT, handlectrlc);
     try {
     do_client(SVRNAME, StringList());
     }
--- testsvr.cpp
+++ testsvr.cpp
@@ -4,6 +4,8 @@
 #include "boost_ipc_svr.h"
 #elif defined(USE_SHMEM)
 #include "shmem_ipc_svr.h"
+#elif defined(USE_PTY)
+#include "pty_ipc_svr.h"
 #else
 #include "stdio_ipc_svr.h"
 #endif
@@ -13,9 +15,16 @@
 
 #include "stringutils.h"
 #include "vectorutils.h"
+#include "util/HiresTimer.h"
 
-#define IPCTESTSIZE 16
-void do_server()
+int g_ctrlcpressed= 0;
+void handlectrlc(int sig)
+{
+    fprintf(stderr, "ctrl-c pressed\n");
+    if (g_ctrlcpressed++ > 2)
+        signal(sig, SIG_DFL);
+}
+void do_server(int argc, char**argv)
 {
 #if defined(USE_SOCKET)
     socket_ipc_server _ipc;
@@ -23,29 +32,46 @@ void do_server()
     boost_ipc_server _ipc;
 #elif defined(USE_SHMEM)
     shmem_ipc_server _ipc;
+#elif defined(USE_PTY)
+    if (argc!=2) {
+        fprintf(stderr, "Usage: testptysvr [ptydevname]\n");
+        return;
+    }
+    pty_ipc_server _ipc(argv[1]);
 #else
     stdio_ipc_server _ipc(STDOUT_FILENO,STDIN_FILENO);
 #endif
 
-    while (true)
+    while (!g_ctrlcpressed)
     {
         DWORD s[2];
         if (!_ipc.read(s, sizeof(s)))
             break;
+
         ByteVector req(s[0]-8);
+        if (s[0]>0x100000) {
+            fprintf(stderr, "svr: %08x %08x\n", s[0], s[1]);
+            break;
+        }
+
+        if (g_ctrlcpressed)
+            break;
         if (!_ipc.read(&req[0], req.size()))
             break;
 
         ByteVector res(s[1]);
 
+        if (g_ctrlcpressed)
+            break;
         if (!_ipc.write(&res[0], res.size()))
             break;
     }
 }
 int main(int argc,char**argv)
 {
+    signal(SIGINT, handlectrlc);
     try {
-    do_server();
+    do_server(argc, argv);
     }
     catch(...)
     {
