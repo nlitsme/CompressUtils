26d915b3765ea670eb33eb3162566870d0419bef
Willem Hengeveld <itsme@xs4all.nl>
2011-10-23 20:54:09 +0200

    compressutils: added makefile for tests, testxph.cpp

---
+++ tests.mk
@@ -0,0 +1,18 @@
+all: testxph testxpr
+testxph: testxph.o stringutils.o dllloader.o
+testxpr: testxpr.o stringutils.o dllloader.o debug.o FileFunctions.o
+
+CXX=g++-mp-4.5
+
+CFLAGS=-Wall -m32 -g -I ../../itsutils/common -D_UNIX -D_NO_RAPI
+LDFLAGS=-m32 -g
+%: %.o
+	$(CXX) $(LDFLAGS) $^ -o $@ -liconv
+%.o: ../../itsutils/common/%.cpp
+	$(CXX) -c $(CFLAGS) $^ -o $@
+
+%.o: %.cpp
+	$(CXX) -c $(CFLAGS) $^ -o $@
+
+clean:
+	$(RM) testxph testxph.o stringutils.o testxpr testxpr.o  dllloader.o debug.o FileFunctions.o
